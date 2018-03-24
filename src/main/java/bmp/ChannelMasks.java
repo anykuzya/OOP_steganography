9c8161f6eb2d86f0a18701ddea84697520aaed7e
anykuzya <aakuznetsova_9@edu.hse.ru>
2018-03-25 01:29:03 +0300

    not multiplier but shift

--- src/main/java/bmp/ChannelMasks.java
+++ src/main/java/bmp/ChannelMasks.java
@@ -1,6 +1,6 @@
 package bmp;
 
-import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
@@ -9,29 +9,46 @@ import java.util.stream.Stream;
 
 import static lombok.AccessLevel.PRIVATE;
 
-public class ChannelMasks {
+@EqualsAndHashCode
+class ChannelMasks {
 
     /**
      * Данная маска задается только в случаях, когда пиксели кодируются номером строки в таблице цветов.
      * В этом случае, остальные все маски равны нулю.
      */
-    private final MaskAndMultiplier colorMapIndex;
+    private final MaskAndShift colorMapIndex;
 
-    private final MaskAndMultiplier red;
+    private final MaskAndShift red;
 
-    private final MaskAndMultiplier green;
+    private final MaskAndShift green;
 
-    private final MaskAndMultiplier blue;
+    private final MaskAndShift blue;
 
-    private final MaskAndMultiplier alpha;
+    private final MaskAndShift alpha;
 
-    public ChannelMasks(long colorMapIndexMask, long redMask, long greenMask, long blueMask, long alphaMask) {
-        this.colorMapIndex = MaskAndMultiplier.ofMask(colorMapIndexMask);
-        this.red = MaskAndMultiplier.ofMask(redMask);
-        this.green = MaskAndMultiplier.ofMask(greenMask);
-        this.blue = MaskAndMultiplier.ofMask(blueMask);
-        this.alpha = MaskAndMultiplier.ofMask(alphaMask);
-        if (channels().noneMatch(MaskAndMultiplier::isActive)) {
+    public long redMask() {
+        return this.red.mask;
+    }
+
+    public long greenMask() {
+        return this.green.mask;
+    }
+
+    public long blueMask() {
+        return this.blue.mask;
+    }
+
+    public long alphaMask() {
+        return this.alpha.mask;
+    }
+
+    ChannelMasks(long colorMapIndexMask, long redMask, long greenMask, long blueMask, long alphaMask) {
+        this.colorMapIndex = MaskAndShift.ofMask(colorMapIndexMask);
+        this.red = MaskAndShift.ofMask(redMask);
+        this.green = MaskAndShift.ofMask(greenMask);
+        this.blue = MaskAndShift.ofMask(blueMask);
+        this.alpha = MaskAndShift.ofMask(alphaMask);
+        if (channels().noneMatch(MaskAndShift::isActive)) {
             throw new IllegalArgumentException("At least one active channel should be provided");
         }
     }
@@ -45,42 +62,43 @@ public class ChannelMasks {
     @NotNull
     public LongUnaryOperator pixelMapper(@NotNull LongUnaryOperator channelMapper) {
         return pixel -> channels()
-                .filter(MaskAndMultiplier::isActive)
+                .filter(MaskAndShift::isActive)
                 .mapToLong(mm -> mapChannel(pixel, mm, channelMapper))
                 .reduce((x, y) -> x | y)
                 .orElseThrow(() -> new IllegalStateException("at least one channel should be active"));
     }
 
-    private Stream<MaskAndMultiplier> channels() {
+    private Stream<MaskAndShift> channels() {
         return Stream.of(this.colorMapIndex, this.red, this.green, this.blue, this.alpha);
     }
 
-    private static long mapChannel(long pixel, MaskAndMultiplier mm, LongUnaryOperator channelMapper) {
-        long normalizedChannelValue = (pixel & mm.mask) / mm.multiplier;
+    private static long mapChannel(long pixel, MaskAndShift mm, LongUnaryOperator channelMapper) {
+        long normalizedChannelValue = (pixel & mm.mask) >>> mm.shift;
         long mappedValue = channelMapper.applyAsLong(normalizedChannelValue);
-        return (mappedValue * mm.multiplier) & mm.mask;
+        return (mappedValue << mm.shift) & mm.mask;
     }
 
     @RequiredArgsConstructor(access = PRIVATE)
-    private static class MaskAndMultiplier {
+    @EqualsAndHashCode
+    private static class MaskAndShift {
         final long mask;
 
-        final long multiplier;
+        final long shift;
 
         boolean isActive() {
-            return mask != 0 && multiplier != 0;
+            return mask != 0;
         }
 
-        static MaskAndMultiplier ofMask(long mask) {
+        static MaskAndShift ofMask(long mask) {
             if (mask == 0) {
-                return new MaskAndMultiplier(0L, 0L);
+                return new MaskAndShift(0L, 0L);
             }
 
-            long multiplier = 1;
-            while (mask / multiplier % 2 == 0) {
-                multiplier *= 2;
+            long shift = 0;
+            while (((mask >>> shift) & 1) == 0) {
+                shift++;
             }
-            return new MaskAndMultiplier(mask, multiplier);
+            return new MaskAndShift(mask, shift);
         }
     }
 }
--- src/test/java/bmp/ChannelMasksTests.java
+++ src/test/java/bmp/ChannelMasksTests.java
@@ -26,6 +26,17 @@ public class ChannelMasksTests {
         verify(this.channelMapper, times(3)).applyAsLong(anyLong());
     }
 
+    @Test
+    public void shouldCorrectlyHandleMasksWithSignBit() {
+        when(this.channelMapper.applyAsLong(0xffffL)).thenReturn(1L);
+
+        long onesMask = 0xffff_0000_0000_0000L;
+        long pixel = -1;
+        ChannelMasks masks = new ChannelMasks(0, onesMask, 0, 0, 0);
+        masks.pixelMapper(this.channelMapper).applyAsLong(pixel);
+
+        verify(this.channelMapper).applyAsLong(0xffffL);
+    }
     @Before
     public void setUp() throws Exception {
         this.channelMapper = mock(LongUnaryOperator.class);
