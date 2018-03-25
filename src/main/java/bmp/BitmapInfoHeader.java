4fe8bb0bc43441eeb24bdaca5db2b96f3675aadd
anykuzya <aakuznetsova_9@edu.hse.ru>
2018-03-25 10:58:36 +0300

    infoheader implemented

--- src/main/java/bmp/BitmapInfoHeader.java
+++ src/main/java/bmp/BitmapInfoHeader.java
@@ -1,67 +1,57 @@
 package bmp;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.nio.ByteBuffer;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static bmp.PixmapType.*;
 import static java.lang.Math.abs;
 import static java.nio.ByteOrder.LITTLE_ENDIAN;
 
+@RequiredArgsConstructor
 public class BitmapInfoHeader {
-//    private final PixmapType type;
-//    private final int width;
-//    private final int height;
-//    private final int bitsPerPixel;
-//    private final ChannelMasks channelMaks;
-//
-//    /**
-//     * содержит ли заголовок корректные (согласованные) и поддерживаемые значения
-//     */
-//    private final boolean isSupported;
-
-    public BitmapInfoHeader(@NotNull ByteBuffer infoBytes) {
-        infoBytes = infoBytes.asReadOnlyBuffer().order(LITTLE_ENDIAN);
-        int size = infoBytes.getInt(0x00);
-        Version version = Version.ofSize(size);
-
-    }
+    private final PixmapType type;
+    private final int width;
+    private final int height;
+    private final int bitsPerPixel;
+    private final ChannelMasks channelMaks;
 
     /**
      * Способ хранения растра.
      */
     @NotNull
     public PixmapType type() {
-        //todo:: impl!
-        return null;
+        return type;
     }
 
     /**
      * Высота растра в пикселях
      */
     public int width() {
-        //todo:: impl!
-        return 0;
+        return width;
     }
 
     /**
      * Ширина растра в пикселях
      */
     public int height() {
-        //todo:: impl!
-        return 0;
+        return height;
     }
 
     /**
      * Ширина пикселя в битах
      */
     public int bitsPerPixel() {
-        //todo:: impl!
-        return 0;
+        return bitsPerPixel;
     }
 
     /**
@@ -70,8 +60,33 @@ public class BitmapInfoHeader {
      */
     @NotNull
     public ChannelMasks channelMaks() {
-        //todo:: impl!
-        return null;
+        return channelMaks;
+    }
+
+    public static BitmapInfoHeader ofBytes(@NotNull ByteBuffer infoBytes) {
+        infoBytes = infoBytes.asReadOnlyBuffer().order(LITTLE_ENDIAN);
+        int size = infoBytes.getInt(0x00);
+        Version version = Version.ofSize(size);
+        if (version == null) {
+            return unsupportedHeader();
+        }
+        Optional<Compression> compressionOptional = version.extractCompression(infoBytes);
+        PixmapType type = compressionOptional.map(Compression::getPixmapType).orElse(UNSUPPORTED);
+        if (type == UNSUPPORTED) {
+            return unsupportedHeader();
+        }
+        Compression compression = compressionOptional.get();
+        int bitsPerPixel = version.extractbitsPerPixel(infoBytes);
+        if (bitsPerPixel < 8 || !compression.isBitCountSuppoted(bitsPerPixel)) {
+            return unsupportedHeader();
+        }
+
+        return new BitmapInfoHeader(type, version.extractWidth(infoBytes), version.extractHeight(infoBytes), bitsPerPixel, version.extractChannelMasks(infoBytes, bitsPerPixel));
+    }
+
+    @NotNull
+    private static BitmapInfoHeader unsupportedHeader() {
+        return new BitmapInfoHeader(UNSUPPORTED, 0, 0, 0, null);
     }
 
     @RequiredArgsConstructor
@@ -88,8 +103,21 @@ public class BitmapInfoHeader {
             }
 
             @Override
-            public Optional<Compression> compression(ByteBuffer bytes) {
+            public int extractbitsPerPixel(ByteBuffer bytes) {
+                return abs(bytes.getShort(0x0a));
+            }
+
+            @Override
+            public Optional<Compression> extractCompression(ByteBuffer bytes) {
                 return Compression.ofValue(0);
+            }
+
+            @Override
+            public ChannelMasks extractChannelMasks(ByteBuffer bytes, int bitsPerPixel) {
+                if (isClrUsed(bytes, bitsPerPixel)) {
+                    return new ChannelMasks((1 << bitsPerPixel) - 1, 0, 0, 0, 0);
+                }
+                return defaultMask(bitsPerPixel);
             }
         },
         V3(40) {
@@ -104,8 +132,21 @@ public class BitmapInfoHeader {
             }
 
             @Override
-            public Optional<Compression> compression(ByteBuffer bytes) {
+            public int extractbitsPerPixel(ByteBuffer bytes) {
+                return abs(bytes.getShort(0x0e));
+            }
+
+            @Override
+            public Optional<Compression> extractCompression(ByteBuffer bytes) {
                 return Compression.ofValue(bytes.getInt(0x10));
+            }
+
+            @Override
+            public ChannelMasks extractChannelMasks(ByteBuffer bytes, int bitsPerPixel) {
+                if (isClrUsed(bytes, bitsPerPixel)) {
+                    return new ChannelMasks((1 << bitsPerPixel) - 1, 0, 0, 0, 0);
+                }
+                return defaultMask(bitsPerPixel);
             }
         },
         V4(108) {
@@ -120,8 +161,20 @@ public class BitmapInfoHeader {
             }
 
             @Override
-            public Optional<Compression> compression(ByteBuffer bytes) {
+            public int extractbitsPerPixel(ByteBuffer bytes) {
+                return abs(bytes.getShort(0x0e));
+            }
+
+            @Override
+            public Optional<Compression> extractCompression(ByteBuffer bytes) {
                 return Compression.ofValue(bytes.getInt(0x10));
+            }
+            @Override
+            public ChannelMasks extractChannelMasks(ByteBuffer bytes, int bitsPerPixel) {
+                if (isClrUsed(bytes, bitsPerPixel)) {
+                    return new ChannelMasks((1 << bitsPerPixel) - 1, 0, 0, 0, 0);
+                }
+                return extractMaskFromBytes(bytes, bitsPerPixel);
             }
         },
         V5(124) {
@@ -136,8 +189,20 @@ public class BitmapInfoHeader {
             }
 
             @Override
-            public Optional<Compression> compression(ByteBuffer bytes) {
+            public int extractbitsPerPixel(ByteBuffer bytes) {
+                return abs(bytes.getShort(0x0e));
+            }
+
+            @Override
+            public Optional<Compression> extractCompression(ByteBuffer bytes) {
                 return Compression.ofValue(bytes.getInt(0x10));
+            }
+            @Override
+            public ChannelMasks extractChannelMasks(ByteBuffer bytes, int bitsPerPixel) {
+                if (isClrUsed(bytes, bitsPerPixel)) {
+                    return new ChannelMasks((1 << bitsPerPixel) - 1, 0, 0, 0, 0);
+                }
+                return extractMaskFromBytes(bytes, bitsPerPixel);
             }
         };
 
@@ -152,27 +217,67 @@ public class BitmapInfoHeader {
 
         public abstract int extractWidth(ByteBuffer bytes);
 
-        public abstract Optional<Compression> compression(ByteBuffer bytes);
+        public abstract int extractbitsPerPixel(ByteBuffer bytes);
 
+        public abstract Optional<Compression> extractCompression(ByteBuffer bytes);
+
+        public abstract ChannelMasks extractChannelMasks(ByteBuffer bytes, int bitsPerPixel);
+
+        private static ChannelMasks extractMaskFromBytes(ByteBuffer bytes, int bitsPerPixel) {
+            int red = bytes.getInt(0x28);
+            int green = bytes.getInt(0x2c);
+            int blue = bytes.getInt(0x30);
+            int alpha = bytes.getInt(0x34);
+            if (red == 0 && green == 0 && blue == 0 && alpha == 0) {
+                return defaultMask(bitsPerPixel);
+            }
+            return new ChannelMasks(0, red, green, blue, alpha);
+        }
+        public boolean isClrUsed(ByteBuffer bytes, int bitsPerPixel) {
+            return (bitsPerPixel <= 8) || (this != CORE && bytes.getInt(0x20) > 0);
+        }
+        private static ChannelMasks defaultMask(int bitsPerPixel) {
+            switch (bitsPerPixel) {
+                case 16:
+                    return new ChannelMasks(0, 0x7c00, 0x03e0, 0x001f, 0);
+                case 32:
+                    return new ChannelMasks(0, 0x00ff_0000L, 0x0000_ff00L, 0x0000_00ffL, 0x0000_0000L);
+                case 24:
+                    return new ChannelMasks(0, 0x00ff_0000L, 0x0000_ff00L, 0x0000_00ffL, 0);
+                case 48:
+                    return new ChannelMasks(0, 0xffff_0000_0000L, 0x0000_ffff_0000L, 0x0000_0000_ffffL, 0x0000_0000_0000L);
+                case 64:
+                    return new ChannelMasks(0, 0xffff_0000_0000_0000L, 0x0000_ffff_0000_0000L, 0x0000_0000_ffff_0000L, 0x0000_0000_0000_0000L);
+                default:
+                    return null;
+            }
+
+        }
     }
 
     @RequiredArgsConstructor
     private enum Compression {
 
-        BI_RGB(0, PIXEL_MATRIX),
-        BI_RLE8(1, RLE),
-        BI_RLE4(2, UNSUPPORTED),
-        BI_BITFIELDS(3, PIXEL_MATRIX),
-        BI_JPEG(4, UNSUPPORTED),
-        BI_PNG(5, UNSUPPORTED),
-        BI_ALPHABITFIELDS(6, PIXEL_MATRIX);
+        BI_RGB(0, PIXEL_MATRIX, Collections.emptySet()),
+        BI_RLE8(1, RLE, Collections.singleton(8)),
+        BI_RLE4(2, UNSUPPORTED, Collections.singleton(4)),
+        BI_BITFIELDS(3, PIXEL_MATRIX, Stream.of(16, 32).collect(Collectors.toSet())),
+        BI_JPEG(4, UNSUPPORTED, Collections.singleton(0)),
+        BI_PNG(5, UNSUPPORTED, Collections.singleton(0)),
+        BI_ALPHABITFIELDS(6, PIXEL_MATRIX, Stream.of(16, 32).collect(Collectors.toSet()));
 
         private final int value;
+        @Getter
         private final PixmapType pixmapType;
+        private final Set<Integer> bitCountRestrictions;
 
         @Nullable
         public static Optional<Compression> ofValue(int value) {
             return Arrays.stream(values()).filter(v -> v.value == value).findAny();
         }
+        public boolean isBitCountSuppoted(int bitCount) {
+            return bitCountRestrictions.isEmpty() || bitCountRestrictions.contains(bitCount);
+        }
+
     }
 }
