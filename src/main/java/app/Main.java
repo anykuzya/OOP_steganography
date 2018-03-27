be5c425b8e3194ca28fb4a0a49d73e3d82406b1f
Anna.Kuznetsova <Anna.Kuznetsova@jetbrains.com>
2018-03-27 09:36:38 +0300

    main refactoring

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,6 +1,7 @@
 package app;
 
 import org.apache.commons.cli.*;
+import org.jetbrains.annotations.NotNull;
 import steganography.Encoding;
 
 import java.io.File;
@@ -18,12 +19,88 @@ public class Main {
             System.out.println("Please, use --help");
             return;
         }
+        Options options = createOptions();
+        CommandLineParser cmdLineParser = new DefaultParser();
+
+        try {
+            CommandLine commandLine = cmdLineParser.parse(options, args);
+            if (commandLine.hasOption("embed")) {
+                runEncodingApplication(commandLine);
+            } else if (commandLine.hasOption("decode")) {
+                runDecodingApplication(commandLine);
+            } else if (commandLine.hasOption("help")) {
+                printHelp();
+            }
+
+        } catch (ParseException | NumberFormatException e) {
+            System.out.println("Error: " + e.getMessage());
+            printHelp();
+
+        } catch (IOException e) {
+            System.out.println("Unexpected IOException: " + e.getMessage());
+        }
+    }
+
+    private static void printHelp() {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp(100, "bmp-steganography", "--HELP--", createOptions(), "--HELP--", true);
+    }
+
+    private static void runDecodingApplication(CommandLine commandLine) throws ParseException {
+        String inputFile = commandLine.getOptionValue("i");
+        String outputFile = commandLine.getOptionValue("o"); // file для расшифрованного текста
+        String encodingName = commandLine.getOptionValue("e");
+
+        if (inputFile == null || outputFile == null) {
+            throw new ParseException("");
+        }
+
+        Encoding encoding = parseEncoding(encodingName);
+
+        DecodingApplication app = new DecodingApplication(new File(inputFile), encoding, new File(outputFile));
+        app.run();
+    }
+
+    @NotNull
+    private static Encoding parseEncoding(String encodingName) throws ParseException {
+        Encoding encoding;
+        if (encodingName == null) return UTF_8;
+        if (encodingName.equalsIgnoreCase("utf-16")) return UTF_16;
+        if (encodingName.equalsIgnoreCase("utf-8")) return UTF_8;
+        if (encodingName.equalsIgnoreCase("us-ascii")) return US_ASCII;
+        else throw new ParseException("wrong encoding key");
+    }
+
+    private static void runEncodingApplication(CommandLine commandLine) throws ParseException, IOException {
+        String inputFile = commandLine.getOptionValue("i");
+        String encodingName = commandLine.getOptionValue("e");
+        String bpc = commandLine.getOptionValue("b");
+        String outputFile = commandLine.getOptionValue("o");
+        String embeddingTextPath = commandLine.getOptionValue("d");
+
+        if (inputFile == null || outputFile == null || embeddingTextPath == null) {
+            throw new ParseException("");
+        }
+
+        Encoding encoding = parseEncoding(encodingName);
+
+        String text2embed = "";
+        text2embed = Files.lines(Paths.get(embeddingTextPath), encoding.getCharset()).collect(Collectors.joining("\n"));
+        int bitsPerChannel = Integer.parseInt(bpc);
+        EncodingApplication app = new EncodingApplication(
+                new File(inputFile), text2embed, encoding, bitsPerChannel, new File(outputFile)
+        );
+        app.run();
+    }
+
+    private static Options createOptions() {
         // --embed или --decode --- режим
         Options options = new Options();
 
         OptionGroup modes = new OptionGroup();
         modes.addOption(new Option("E", "embed", false, "встроить текст в изображение"));
         modes.addOption(new Option("D", "decode", false,"извлечь текст из изображения"));
+        modes.addOption(new Option("H", "help", false, "вывести информацию об использовании"));
         modes.setRequired(true);
         options.addOptionGroup(modes);
 
@@ -43,55 +120,6 @@ public class Main {
         options.addOption("b", true, "количество бит, вшиваемых в маску каждого канала." +
                 "строго степень 2, не рекомендуется выше 4, если битность изображения меньше 32");
 
-        CommandLineParser cmdLineParser = new DefaultParser();
-        try {
-            CommandLine commandLine = cmdLineParser.parse(options, args);
-            if (commandLine.hasOption("embed")) {
-                String inputFile = commandLine.getOptionValue("i");
-                String encodingName = commandLine.getOptionValue("e");
-                String bpc = commandLine.getOptionValue("b");
-                String outputFile = commandLine.getOptionValue("o");
-                String embeddingTextPath = commandLine.getOptionValue("d");
-
-                if (inputFile == null || outputFile == null || embeddingTextPath == null) {
-                    throw new ParseException("");
-                }
-
-                Encoding encoding = (encodingName == null || encodingName.equalsIgnoreCase("UTF-16")) ? UTF_16 :
-                                    (encodingName.equalsIgnoreCase("UTF-8") ? UTF_8 : US_ASCII);
-
-                String text2embed = "";
-                try {
-                    text2embed = Files.lines(Paths.get(embeddingTextPath), encoding.getCharset()).collect(Collectors.joining("\n"));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                int bitsPerChannel = Integer.getInteger(bpc, 2);
-                EncodingApplication app = new EncodingApplication(
-                        new File(inputFile), text2embed, encoding, bitsPerChannel, new File(outputFile)
-                );
-                app.run();
-
-            } else if (commandLine.hasOption("decode")) {
-                String inputFile = commandLine.getOptionValue("i");
-                String outputFile = commandLine.getOptionValue("o"); // file для расшифрованного текста
-                String encodingName = commandLine.getOptionValue("e");
-
-                if (inputFile == null || outputFile == null) {
-                    throw new ParseException("");
-                }
-
-                Encoding encoding = (encodingName == null || encodingName.equalsIgnoreCase("UTF-16")) ? UTF_16 :
-                        (encodingName.equalsIgnoreCase("UTF-8") ? UTF_8 : US_ASCII);
-
-                DecodingApplication app = new DecodingApplication(new File(inputFile), encoding, new File(outputFile));
-                app.run();
-            }
-
-        } catch (ParseException e) {
-            HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp(100, "bmp-steganography","--HELP--", options, "--HELP--",  true);
-
-        }
+        return options;
     }
 }
